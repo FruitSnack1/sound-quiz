3889eca1019ad4ce644a8b4c59eb6e1af729bba2
fruitsnack1 <tomas.ryzner@email.cz>
2020-02-16 11:56:01 +0100

    final changes

--- index.js
+++ index.js
@@ -13,7 +13,17 @@ app.use(express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded())
 
 app.get('/', (req,res) =>{
-    res.sendFile(pth.join(__dirname+'index.html'))
+    res.sendFile(path.join(__dirname+'index.html'))
 })
 
-app.listen(80,'192.168.1.102', () => console.log('Server Started'))
+app.post('/', (req, res) =>{
+    MongoClient.connect('mongodb://127.0.0.1:27017/sound', async (err,client) =>{
+        if (err) return console.log('Unable to connect to the Server', err)
+        const db = client.db('sound')
+        const result = await db.collection('results').insertOne(req.body)
+        console.log(result.ops);
+        res.sendStatus(200)
+    })
+})
+
+app.listen(80, '192.168.0.54', () => console.log('Server Started'))
--- main.js
+++ main.js
@@ -1,26 +1,69 @@
-const slides = ['login', 'front.jpg', 'top.jpg']
-
+let result = {}
 let stage = 0
 
-function next(){
+function next(value) {
     stage++
 
     switch (stage) {
+        case 0:
+            $('.end').hide()
+            $('.login').fadeIn(300)
+            break
         case 1:
+            result['personalNumber'] = $('#personalNumber').val()
             $('.login').hide()
-            $('.play').show()
+            $('.play').fadeIn(300)
+            $('#img1').show()
             break;
         case 2:
-            $('.question-image').css('background-image', 'url("images/top.png")')
-            $('.zone-container').hide()
-            $('.zone-container-2').css('display','flex')
-            $('.zone').css('height', '100%')
-            $('.zone').css('width', '18%')
+            result['driverSeat'] = value
+            $('#img1').hide()
+            $('#img2').fadeIn(300)
             break
         case 3:
-            $('.question-image').hide()
-            $('.slider').css('display', 'flex')
+            result['driverSeatTop'] = value
+            $('#img2').hide()
+            $('#slider1').css('display', 'flex').hide().fadeIn(300)
+            break
+        case 4:
+            result['driverSeatOverall'] = parseInt(value)
+            $('#slider1').hide()
+            $('#img3').fadeIn(300)
+            break
+        case 5:
+            result['backSeat'] = value
+            $('#img3').hide()
+            $('#img4').fadeIn(300)
+            break
+        case 6:
+            result['backSeatTop'] = value
+            $('#img4').hide()
+            $('#slider2').css('display', 'flex').hide().fadeIn(300)
+            break
+        case 7:
+            result['backSeatOverall'] = parseInt(value)
+            $('#slider2').hide()
+            $('.play').hide()
+            $('.end').fadeIn(300)
+            postResult()
+            break
         default:
-            break;
+            break
     }
+}
+
+function postResult() {
+    $.post({
+        method: 'POST',
+        url: location.href,
+        data: result
+    })
+    setTimeout(() => {
+        result = {}
+        stage = -1
+        $('#personalNumber').val('')
+        $('#overall1').val(0)
+        $('#overall2').val(0)
+        next()
+    }, 1500)
 }
